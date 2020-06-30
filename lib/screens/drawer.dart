0026b6a922a1961146ef75dcb621368c6d95850b
Sarvesh Nath Tiwari <sarveshsandilya01@gmail.com>
2020-06-30 23:14:22 +0530

    Custom Color added

--- lib/screens/drawer.dart
+++ lib/screens/drawer.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
+Map<int, Color> color = {
+ 50: Color.fromRGBO(255, 92, 87, .1),
+100: Color.fromRGBO(255, 92, 87, .2),
+200: Color.fromRGBO(255, 92, 87, .3),
+300: Color.fromRGBO(255, 92, 87, .4),
+400: Color.fromRGBO(255, 92, 87, .5),
+500: Color.fromRGBO(255, 92, 87, .6),
+600: Color.fromRGBO(255, 92, 87, .7),
+700: Color.fromRGBO(255, 92, 87, .8),
+800: Color.fromRGBO(255, 92, 87, .9),
+900: Color.fromRGBO(255, 92, 87, 1),
+};
+
+MaterialColor colorCustom = MaterialColor(0xff17252A, color);
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
 
   @override
@@ -9,10 +24,9 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'NewsApp',
       theme: new ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: colorCustom,
       ),
       home: new MyHomePage(title: 'My NewsApp'),
     );
   }
 }
-
