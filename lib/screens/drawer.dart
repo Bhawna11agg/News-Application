9f8d638379d9889c9f601a15736ed124f3e9c1db
Sarvesh Nath Tiwari <sarveshsandilya01@gmail.com>
2020-06-30 23:08:15 +0530

    Update drawer.dart

--- lib/screens/drawer.dart
+++ lib/screens/drawer.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'NewsApp',
+      theme: new ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: new MyHomePage(title: 'My NewsApp'),
+    );
+  }
+}
 
